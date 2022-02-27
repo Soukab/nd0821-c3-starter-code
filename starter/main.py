ddb6ff38a5d877960ae0e5d8314f5957da2227a6
Soukaina Abelhad <soukaina.abelhad@truecaller.com>
2022-02-27 11:24:21 +0100

    add get and post methods plus the tests

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,119 @@
 # Put the code for your API here.
+import pandas as pd
+from pydantic import BaseModel
+from fastapi import FastAPI, Body
+import pickle
+import os
+
+from starter.ml.data import process_data, categorical_features
+from starter.ml.model import inference
+
+
+class CensusData(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
+
+
+keep_cat = ["marital-status", "race", "relationship", "sex"]  # Limit sparsity
+
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
+
+def load_pickle(file):
+    with open(file, "rb") as f:
+        pickle_object = pickle.load(f)
+    return pickle_object
+
+
+model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "model/model.pkl")
+encoder_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "model/encoder.pkl"
+)
+lb_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "model/labeler.pkl")
+
+# Create the app object
+app = FastAPI()
+
+# Loading pickles
+classifier = load_pickle(model_path)
+encoder = load_pickle(encoder_path)
+labeler = load_pickle(lb_path)
+
+@app.get("/")
+async def say_hello():
+    return "Hello World!"
+
+
+@app.post("/predict_salary/")
+def predict_salary(
+        data: CensusData = Body(
+            None,
+            example={
+                "age": 39,
+                "workclass": "State-gov",
+                "fnlgt": 77516,
+                "education": "Bachelors",
+                "education_num": 13,
+                "marital_status": "Never-married",
+                "occupation": "Adm-clerical",
+                "relationship": "Not-in-family",
+                "race": "White",
+                "sex": "Male",
+                "capital_gain": 2174,
+                "capital_loss": 0,
+                "hours_per_week": 40,
+                "native_country": "United-States",
+            },
+        )
+):
+
+    data_dict = {
+        "age": [data.age],
+        "workclass": [data.workclass],
+        "fnlgt": [data.fnlgt],
+        "education": [data.education],
+        "education-num": [data.education_num],
+        "marital-status": [data.marital_status],
+        "occupation": [data.occupation],
+        "relationship": [data.relationship],
+        "race": [data.race],
+        "sex": [data.sex],
+        "capital-gain": [data.capital_gain],
+        "capital-loss": [data.capital_loss],
+        "hours-per-week": [data.hours_per_week],
+        "native-country": [data.native_country],
+    }
+
+    data = pd.DataFrame.from_dict(data_dict)
+
+    cat_features = categorical_features(data)
+
+    X, _, _, _ = process_data(
+        data,
+        categorical_features=cat_features,
+        training=False,
+        encoder=encoder,
+        keep_cat=keep_cat,
+    )
+
+    prediction = inference(classifier, X)
+    prediction = labeler.inverse_transform(prediction)
+    print('The predicted salary is :', prediction)
+    return prediction[0]
