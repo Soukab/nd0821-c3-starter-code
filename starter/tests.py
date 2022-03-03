354ff4b35479f4505c1263e46e8f97dc395056cd
Soukaina Abelhad <soukaina.abelhad@truecaller.com>
2022-03-03 13:07:28 +0100

    fix errors

--- starter/tests.py
+++ starter/tests.py
@@ -7,7 +7,7 @@ from starter.ml.data import process_data
 from starter.ml.model import train_model, compute_model_metrics, inference
 
 
-simp_path = 'starter/data/clean_census.csv'
+simp_path = 'nd0821-c3-starter-code/starter/data/clean_census.csv'
 abs_path = os.path.abspath(simp_path)# Add code to load in the data.
 data = pd.read_csv(abs_path)
 
