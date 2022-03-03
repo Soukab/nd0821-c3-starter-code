fd1427b4603ac574a98372bf3539bac9f08d1def
Soukaina Abelhad <soukaina.abelhad@truecaller.com>
2022-03-03 12:54:18 +0100

    fix errors

--- starter/tests.py
+++ starter/tests.py
@@ -7,8 +7,9 @@ from starter.ml.data import process_data
 from starter.ml.model import train_model, compute_model_metrics, inference
 
 
-# Add code to load in the data.
-data = pd.read_csv("data/clean_census.csv")
+simp_path = 'data/clean_census.csv'
+abs_path = os.path.abspath(simp_path)# Add code to load in the data.
+data = pd.read_csv(abs_path)
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
